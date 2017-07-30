c935a6a3aa791da90898e9fe50359ef00ba1714c
vain0x <vainzerox@gmail.com>
2017-07-31 00:25:16 +0900

    WIP

---
+++ VainZero.Timermaid.Desktop/Timermaid/ScheduleLists/ScheduleView.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+using VainZero.Misc;
+using VainZero.Timermaid.Data.Entity;
+
+namespace VainZero.Timermaid.ScheduleLists
+{
+    public sealed class ScheduleView
+        : BindableBase
+    {
+        public Schedule Source { get; }
+
+        void SetProperty<X>(X original, Action<X> set, X value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<X>.Default.Equals(original, value)) return;
+            set(value);
+            RaisePropertyChanged(propertyName);
+        }
+
+        public string Name
+        {
+            get => Source.Name;
+            set => SetProperty(Name, x => Source.Name = x, value);
+        }
+
+        public DateTime DueTime
+        {
+            get => Source.DueTime;
+            set => SetProperty(DueTime, x => Source.DueTime = x, value);
+        }
+
+        public string FilePath
+        {
+            get => Source.FilePath;
+            set => SetProperty(FilePath, x => Source.FilePath = x, value);
+        }
+
+        public string Argument
+        {
+            get => Source.Argument;
+            set => SetProperty(Argument, x => Source.Argument = x, value);
+        }
+
+        bool IsActive => DueTime > DateTime.Now;
+
+        public ScheduleViewStatus Status
+        {
+            get => ScheduleViewStatusModule.ToView(Source.Status, IsActive);
+            set => SetProperty(Status, x => Source.Status = ScheduleViewStatusModule.FromView(x), value);
+        }
+
+        public ScheduleView()
+            : this(new Schedule())
+        {
+        }
+
+        public ScheduleView(Schedule source)
+        {
+            Source = source;
+        }
+    }
+}
---
+++ VainZero.Timermaid.Desktop/Timermaid/ScheduleLists/ScheduleViewStatus.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VainZero.Misc;
+using VainZero.Timermaid.Data.Entity;
+
+namespace VainZero.Timermaid.ScheduleLists
+{
+    public enum ScheduleViewStatus
+    {
+        Running,
+        Stopped,
+        Disabled,
+        Hidden,
+    }
+
+    public static class ScheduleViewStatusModule
+    {
+        public static ScheduleViewStatus ToView(ScheduleStatus status, bool isRunning)
+        {
+            switch (status)
+            {
+                case ScheduleStatus.Enabled:
+                    return isRunning ? ScheduleViewStatus.Running : ScheduleViewStatus.Stopped;
+                case ScheduleStatus.Disabled:
+                    return ScheduleViewStatus.Disabled;
+                case ScheduleStatus.Hidden:
+                    return ScheduleViewStatus.Hidden;
+                default:
+                    throw new InvalidValueException(status);
+            }
+        }
+
+        public static ScheduleStatus FromView(ScheduleViewStatus status)
+        {
+            switch (status)
+            {
+                case ScheduleViewStatus.Running:
+                case ScheduleViewStatus.Stopped:
+                    return ScheduleStatus.Enabled;
+                case ScheduleViewStatus.Disabled:
+                    return ScheduleStatus.Disabled;
+                case ScheduleViewStatus.Hidden:
+                    return ScheduleStatus.Hidden;
+                default:
+                    throw new InvalidValueException(status);
+            }
+        }
+    }
+}
