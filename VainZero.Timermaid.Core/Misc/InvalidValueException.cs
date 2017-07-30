995323fdc007725304c974d60f6cef42438ffeeb
vain0x <vainzerox@gmail.com>
2017-07-31 00:09:42 +0900

    Add InvalidValueException

---
+++ VainZero.Timermaid.Core/Misc/InvalidValueException.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VainZero.Misc
+{
+    public sealed class InvalidValueException
+        : InvalidOperationException
+    {
+        public object Value { get; }
+
+        static string CreateMessage(object value)
+        {
+            return
+                value == null
+                    ? "Invalid null."
+                    : $"Invalid value: {value}.";
+        }
+
+        public InvalidValueException(object value)
+            : base(CreateMessage(value))
+        {
+            Value = value;
+        }
+
+        public InvalidValueException(object value, Exception innerException)
+            : base(CreateMessage(value), innerException)
+        {
+            Value = value;
+        }
+    }
+}
