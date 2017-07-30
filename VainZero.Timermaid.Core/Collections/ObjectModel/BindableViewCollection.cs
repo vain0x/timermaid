5f66ba69be0b8d567838462dd38f05c4c053a5d1
vain0x <vainzerox@gmail.com>
2017-07-31 00:31:29 +0900

    WIP

---
+++ VainZero.Timermaid.Core/Collections/ObjectModel/BindableViewCollection.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VainZero.Collections.ObjectModel
+{
+    public abstract class BindableViewCollection<TSource, TTarget>
+        : BindableCollection<TTarget>
+        where TTarget : INotifyPropertyChanged
+    {
+        protected abstract TSource GetSource(TTarget target);
+
+        ICollection<TSource> Sources { get; }
+
+        protected override void OnAdded(TTarget item)
+        {
+            Sources.Add(GetSource(item));
+            base.OnAdded(item);
+        }
+
+        protected override void OnRemoved(TTarget item)
+        {
+            Sources.Remove(GetSource(item));
+            base.OnRemoved(item);
+        }
+
+        public BindableViewCollection(IEnumerable<TTarget> targets, ICollection<TSource> sources)
+            : base(targets)
+        {
+            Sources = sources;
+        }
+    }
+}
