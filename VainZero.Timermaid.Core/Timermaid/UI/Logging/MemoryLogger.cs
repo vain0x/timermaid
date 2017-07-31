e80ddc0c60dfb737378ef9f4c6ad2f7589d5375a
vain0x <vainzerox@gmail.com>
2017-07-31 23:39:30 +0900

    Add ILogger

---
+++ VainZero.Timermaid.Core/Timermaid/UI/Logging/MemoryLogger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VainZero.Collections.ObjectModel;
+
+namespace VainZero.Timermaid.UI.Logging
+{
+    public sealed class MemoryLogger
+        : ILogger
+    {
+        List<LogItem> Items { get; } = new List<LogItem>();
+
+        object LockObject { get; } = new object();
+
+        public void Add(string message)
+        {
+            lock (LockObject)
+            {
+                Items.Add(new LogItem(message, DateTime.UtcNow));
+            }
+        }
+
+        public LogItem[] ToArray()
+        {
+            lock (LockObject)
+            {
+                return Items.ToArray();
+            }
+        }
+    }
+}
